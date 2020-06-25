add1d1001a19a3ee3bc9708a24296948e8817231
franko <francoastorga999@gmail.com>
2020-06-25 16:21:46 -0300

    Agrego Ranking hecho con query , falta la vista

--- src/main/java/com/unla/Grupo14OO22020/repositories/IPedidoRepository.java
+++ src/main/java/com/unla/Grupo14OO22020/repositories/IPedidoRepository.java
@@ -1,15 +1,26 @@
 package com.unla.Grupo14OO22020.repositories;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.unla.Grupo14OO22020.entities.Pedido;
 
 @Repository("pedidoRepository")
 public interface IPedidoRepository extends JpaRepository<Pedido, Serializable>{
 	
 	public abstract Pedido findByIdPedido(int idPedido);
+	
+	@Query(nativeQuery=true,value="select (p.cantidad*pr.precio) from pedido p inner join producto pr on p.producto_id_producto=pr.id_producto group by p.id_pedido;")
+	public abstract List<Object> calcularSubtotal();
+	
+	@Query(nativeQuery=true,value="select sum(cantidad_actual) from Lote where producto_id_producto=(:idProducto) and id_local=(:idLocal) group by producto_id_producto,id_local")
+	public abstract int StockLocal(int idProducto,int idLocal);
+	
+	@Query(nativeQuery=true,value="select sum(p.cantidad) as ranking,pr.nombre,pr.descripcion,pr.precio from Pedido p inner join Producto pr on pr.id_producto=p.producto_id_producto group by p.producto_id_producto order by ranking desc;")
+	public abstract List<String> Ranking();
 
 }
